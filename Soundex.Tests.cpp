c4664427c4a9b7f29dd9bdc8314e7627ee978756
sakthi1989 <51967798+sakthi1989@users.noreply.github.com>
2024-07-07 16:24:47 +0530

    UpdateSoundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -4,6 +4,10 @@
 TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
  //AAA
   char soundex[5];
-  generateSoundex("AX", soundex);
+ generateSoundex("Sakthi", soundex);
+ generateSoundex("Hhow", soundex);
+ generateSoundex("are", soundex);
+ generateSoundex("yyou", soundex);
+ 
  // ASSERT_EQ(soundex,"A200");
 }
