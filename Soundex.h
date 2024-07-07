bb1d56e8af35879cec1ee0f09a7df3aa826d4482
sakthi1989 <51967798+sakthi1989@users.noreply.github.com>
2024-07-07 16:35:58 +0530

    UpdateSoundex.h

--- Soundex.h
+++ Soundex.h
@@ -21,14 +21,11 @@ char getSoundexCode(char c) {
 void generateSoundex(const char *name, char *soundex) {
     memset(soundex, '0', 4);
     soundex[0] = toupper(name[0]);
-    int sIndex = 1;
-    int i = 1;
+    int sIndex = 1, i = 1;
     while(i < strlen(name) && sIndex < 4){
         char code = getSoundexCode(name[i]);
         if (code != '0' && code != soundex[sIndex - 1]) 
-        {
             soundex[sIndex++] = code;
-        }
         i++;}
     soundex[4] = '\0';}
 
